78e15caa3bb366341739ecdd05baa4d63d8e2e38
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2019-01-13 22:32:55 -0500

    Update BaseTwoFormatter.java

--- src/BaseTwoFormatter.java
+++ src/BaseTwoFormatter.java
@@ -3,32 +3,23 @@ public class BaseTwoFormatter implements NumberFormatter
 {
 	public String format(int n)
 	{
-String s = "";
+		String s = "";
         
         while (n/2 != 0)
         {
             s += n%2;
             n /= 2;
         }
-        
-        return s + n%2;
+        String reverse = "" + s + n%2;
+    	
+    	String finished = "";
+    	for(int i=0;i<reverse.length();i++)
+    	{
+    		int reverser = (reverse.length()-1)-i;
+
+    		finished += reverse.charAt(reverser);
+    	}
+    	return finished;
 	}
+	
 }
-/*
-String s = new Integer(n).toString();
-int length = s.length();
-
-String reverse = "";
-for(int i =1; i<length; i++)
-{
-	if()
-}
-String finished = "";
-for(int i=0; i<reverse.length(); i++)
-{
-	int jawn = (reverse.length()-1)-i;
-
-	finished += reverse.charAt(jawn);
-}
-return finished;
-*/
